4e1ea1728b03a8474aa5a076e46c7216b2d07990
Brian Cooper <brian@brian-cooper.com>
2026-02-18 10:54:49 -0500

    chore: clarify auth base path comment and knip ignore reason

--- knip.config.ts
+++ knip.config.ts
@@ -34,6 +34,7 @@ const knipConfig: KnipConfig = {
     // TODO switch to testcontainers (unstable behavior with Bun/Docker), then remove below
     "@testcontainers/postgresql",
     "testcontainers",
+    // Used in test factories (src/test/), which are in the ignore list
     "@faker-js/faker",
   ],
   ignoreBinaries: [],
